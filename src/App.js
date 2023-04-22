ae30d87fd141ab2f23b306fc2f12c0638a902b28
Fellipe Marcel <fmrclms@gmail.com>
2023-04-22 11:03:13 -0300

    requisito 12 completo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import DetailsPage from './pages/DetailsPage';
+import Checkout from './pages/Checkout';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Route exact path="/" component={ Home } />
         <Route exact path="/shoppingcart" component={ ShoppingCart } />
         <Route exact path="/product-details/:id" component={ DetailsPage } />
+        <Route exact path="/checkout" component={ Checkout } />
       </Switch>
     );
   }
